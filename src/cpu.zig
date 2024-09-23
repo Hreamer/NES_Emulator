908a217d35671f6bc11e648ea60c3a0ec0e9fafa
Hudson Reamer <Hudson.Reamer@rockwellautomation.com>
2024-09-23 11:37:22 -0500

    added Pop/Push to CPU and also added writeByte to memory

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
 const Memory = @import("memory.zig");
+const Instructions = @import("instructions.zig");
 
 const CPU = struct {
+    //Registers
     PC: u16,
     SP: u8,
-    Status: u8, //read the docs for this each bit matters
+    Status: u8 = 0b00100000, //read the docs for this each bit matters
     Accumulator: u8,
     X: u8,
     Y: u8,
@@ -12,16 +14,26 @@ const CPU = struct {
     pub fn executeInstr(self: CPU) !void {
         //remember that we need to check for interrupts every step which includes before the next instruction is run
 
-        const instruction = Memory.memorySpace.getByte(self.PC);
+        const instruction = Instructions.decode(self, Memory.memorySpace.getByte(self.PC));
 
         //if there was no error than we handle the returned instruction otherwise propagate the error upward
         if (instruction) |instr| {
             //time to start disecting the command
-            _ = instr; //del later
+            _ = instr;
         } else |err| {
             return err;
         }
 
         return void;
+    }
+
+    pub fn stackPush(self: CPU, value: u8) void {
+        Memory.memorySpace.writeByte(0x100 | @as(u16, self.SP), value);
+        self.SP -%= 1;
+    }
+
+    pub fn stackPop(self: CPU) !u8 {
+        self.SP +%= 1;
+        return Memory.memorySpace.getByte(0x100 | @as(u16, self.SP));
     }
 };
--- src/memory.zig
+++ src/memory.zig
@@ -4,10 +4,22 @@ const Memory = struct {
     MemSpace: [65535]u8 = std.mem.zeroes([65535]u8),
 
     pub fn getByte(self: Memory, location: u16) !u8 {
-        if (location >= self.MemSpace.len) {
+        if (location > self.MemSpace.len) {
+            return anyerror;
+        } else if (0 > location) {
             return anyerror;
         } else {
             return self.MemSpace[location];
+        }
+    }
+
+    pub fn writeByte(self: Memory, location: u16, value: u8) !void {
+        if (location > self.MemSpace.len) {
+            return anyerror;
+        } else if (0 > location) {
+            return anyerror;
+        } else {
+            self.MemSpace[location] = value;
         }
     }
 };
